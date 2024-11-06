ae3a97195329668ef1cc64df3605ba1a1ebf8c3a
niceman1234man <yihunietarekegn18@gmail.com>
2024-11-05 20:49:55 -0800

    signup

--- Client/frontend/src/Pages/SignUp.jsx
+++ Client/frontend/src/Pages/SignUp.jsx
@@ -1,9 +1,94 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
 
 function SignUp() {
-  return (
-    <div>SignUp</div>
-  )
+  const navigate = useNavigate();
+  const [inputValue, setInputValue] = useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+  const {email,password,username}=inputValue;
+  const handleOnchange = (e) => {
+    const { name, value } = e.target;
+    setInputValue({ ...inputValue, [name]: value });
+  };
+  const onErrorHandle = (err) => {
+    toast.error(err, { position: "bottom-left" });
+  };
+  const onHandleSuccess = (msg) => {
+    toast.success(msg, { position: "bottom-right" });
+  };
+  const onSubmithandle = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(
+        "http://localhost:4000/signup",
+        { ...inputValue },
+        { withCredentials: true }
+      );
+      const { success, message } = data;
+      if (success) {
+        onHandleSuccess(message);
+        setTimeout(() => {
+          navigate("/");
+        }, 1000);
+      } else {
+        onErrorHandle(message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    setInputValue({ ...inputValue,
+         email: "", 
+         password: "", 
+         username: "" });
+  };
+
+  return(
+    <div >
+      <h2>Signup Account</h2>
+      <form onSubmit={onSubmithandle}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            placeholder="Enter your email"
+            onChange={handleOnchange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Username</label>
+          <input
+            type="text"
+            name="username"
+            value={username}
+            placeholder="Enter your username"
+            onChange={handleOnchange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            placeholder="Enter your password"
+            onChange={handleOnchange}
+          />
+        </div>
+        <button type="submit">Submit</button>
+        <span>
+          Already have an account? <Link to={"/login"}>Login</Link>
+        </span>
+      </form>
+      <ToastContainer />
+    </div>
+  );
 }
 
-export default SignUp
+export default SignUp;
