7e3a07f7f1dd8494d7e69c88b736d66a73d8eab1
niceman1234man <yihunietarekegn18@gmail.com>
2024-11-05 20:48:34 -0800

    home

--- Client/frontend/src/Pages/Home.jsx
+++ Client/frontend/src/Pages/Home.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React,{useEffect,useState}from 'react'
+import axios from 'axios'
+import {useNavigate} from 'react-router-dom'
+import {ToastContainer,toast} from 'react-toastify'
+import {useCookies} from 'react-cookie'
+const navigate =useNavigate();
+const [cookies,removeCookie]=useCookies([]);
+const [username,setUsername]=useState("");
+useEffect(()=>{
+    const verifyCookie=async()=>{
+        if(!cookies.token){
+            navigate('/login');
+        }
+        const data=await axios.post("http://localhost:4000",{},{
+            withCredentials:true,
+        });
+     const {status,user}=data;
+     setUsername(user);
+     return status ? toast(`Hello ${user}`,{position:"top-right"}) :
+     removeCookie("token",navigate('/login'));
+    }
+    verifyCookie();
 
+},[cookies,navigate,removeCookies]);
+const logout=()=>{
+    removeCookie("token");
+    navigate('/signup');
+}
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <>
+      <div >
+        <h4>
+          {" "}
+          Welcome <span>{username}</span>
+        </h4>
+        <button onClick={logout}>LOGOUT</button>
+      </div>
+      <ToastContainer />
+    </>
+  );
 }
 
 export default Home
