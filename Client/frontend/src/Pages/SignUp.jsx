91e3a3ed293d61d015bf4376ffdbf6c8e1b20055
niceman1234man <yihunietarekegn18@gmail.com>
2024-11-06 20:53:02 -0800

    signup

--- Client/frontend/src/Pages/SignUp.jsx
+++ Client/frontend/src/Pages/SignUp.jsx
@@ -16,7 +16,7 @@ function SignUp() {
     setInputValue({ ...inputValue, [name]: value });
   };
   const onErrorHandle = (err) => {
-    toast.error(err, { position: "bottom-left" });
+    toast.error(err, { position:"top-right" });
   };
   const onHandleSuccess = (msg) => {
     toast.success(msg, { position: "bottom-right" });
@@ -48,10 +48,10 @@ function SignUp() {
   };
 
   return(
-    <div >
-      <h2>Signup Account</h2>
+    <div className="w-[50%] mx-auto flex flex-col items-center justify-center bg-cyan-700 p-4 my-4">
+      <h2 className="p-2">Signup Account</h2>
       <form onSubmit={onSubmithandle}>
-        <div>
+        <div className="p-2">
           <label htmlFor="email">Email</label>
           <input
             type="email"
@@ -59,9 +59,10 @@ function SignUp() {
             value={email}
             placeholder="Enter your email"
             onChange={handleOnchange}
+            className="ml-4 p-2"
           />
         </div>
-        <div>
+        <div className="p-2">
           <label htmlFor="email">Username</label>
           <input
             type="text"
@@ -69,9 +70,10 @@ function SignUp() {
             value={username}
             placeholder="Enter your username"
             onChange={handleOnchange}
+            className="ml-4 p-2"
           />
         </div>
-        <div>
+        <div className="p-2">
           <label htmlFor="password">Password</label>
           <input
             type="password"
@@ -79,12 +81,13 @@ function SignUp() {
             value={password}
             placeholder="Enter your password"
             onChange={handleOnchange}
+            className="ml-4 p-2"
           />
         </div>
-        <button type="submit">Submit</button>
-        <span>
-          Already have an account? <Link to={"/login"}>Login</Link>
-        </span>
+        <button type="submit" className="p-2 bg-slate-500">Submit</button>
+        <p className="p-2">
+          Already have an account? <Link to={"/login"} className="bg-emerald-400 p-2">Login</Link>
+        </p>
       </form>
       <ToastContainer />
     </div>
